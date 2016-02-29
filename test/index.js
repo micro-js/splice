fb01d4f82e0fc123ef6c318f8e469d971438d8fa
Andrew Shaffer <andrew@weo.io>
2016-02-29 12:35:04 -0800

    conform more to standard .splice()

--- test/index.js
+++ test/index.js
@@ -14,6 +14,8 @@ test('should work', function (t) {
   t.deepEqual(splice([1, 2, 3], 1, 1, 5), [1, 5, 3])
   t.deepEqual(splice([1, 2, 3], 0, 1, 5), [5, 2, 3])
   t.deepEqual(splice([1, 2, 3], 2, 1, 5), [1, 2, 5])
+  t.deepEqual(splice([1, 2, 3], 3, 1, 5), [1, 2, 3, 5])
+  t.deepEqual(splice([1, 2, 3], -1, 1, 5), [1, 2, 5])
 
   t.end()
 })
